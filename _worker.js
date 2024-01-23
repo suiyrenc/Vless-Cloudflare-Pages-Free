462b70e87905d056d2d9016ba0cf8a97893c0c15
suiyrenc <137814377+suiyrenc@users.noreply.github.com>
2024-01-23 03:33:51 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = '652da35d-b59d-4ab0-af59-cb90066dcc10';
+let userID = '5d62c7fb-7642-4d0f-9423-7d7e7b4329d3';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
